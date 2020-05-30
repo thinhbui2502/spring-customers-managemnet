a302e8c37397f963ca59b3c1b819e0083dee037b
thinhbui2502 <buiducthinh2502@gmail.com>
2020-05-30 10:21:06 +0700

    add file customer-management

---
+++ src/main/java/cg/wbd/grandemonstration/model/CustomerController.java
@@ -0,0 +1,29 @@
+package cg.wbd.grandemonstration.model;
+
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.CustomerServiceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+//    private CustomerService customerService = CustomerServiceFactory.getInstance();
+
+    @GetMapping("/customers")
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("customers/list.jsp");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+}
